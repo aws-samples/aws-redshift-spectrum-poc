6fafece98de8b530f6630ccbeb6fafd1283f1cc7
dylan-tong-aws <30326366+dylan-tong-aws@users.noreply.github.com>
2017-09-17 15:48:21 -0700

    Update load-dimension-data.sql

--- sql-scripts/load-dimension-data.sql
+++ sql-scripts/load-dimension-data.sql
@@ -1,9 +1,9 @@
 set autocommit=on;
 
 copy customer from 's3://awssampledbuswest2/ssbgz/customer' 
-credentials 'aws_access_key_id=<Your-Access-Key-ID>;aws_secret_access_key=<Your-Secret-Access-Key>' 
+iam_role 'arn:aws:iam::<aws-account-id>:role/<role-name>'
 gzip region 'us-west-2';
 
 copy dwdate from 's3://awssampledbuswest2/ssbgz/dwdate' 
-credentials 'aws_access_key_id=<Your-Access-Key-ID>;aws_secret_access_key=<Your-Secret-Access-Key>' 
+credentials 'arn:aws:iam::<aws-account-id>:role/<role-name>'
 gzip region 'us-west-2';
