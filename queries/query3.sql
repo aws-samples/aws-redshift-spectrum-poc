221df71e10f3617a418f3e125a4e224eefc9ee15
dylan-tong-aws <30326366+dylan-tong-aws@users.noreply.github.com>
2017-09-26 18:14:46 -0700

    Create query3.sql

---
+++ queries/query3.sql
@@ -0,0 +1,12 @@
+SELECT c.c_name, c.c_mktsegment, t.prettyMonthYear, SUM(uv.adRevenue)
+FROM clickstream.uservisits_csv10 as uv
+RIGHT OUTER JOIN 
+customer as c 
+ON c.c_custkey = uv.customer
+INNER JOIN 
+(SELECT DISTINCT d_yearmonthnum, (d_month||','||d_year) as prettyMonthYear 
+FROM dwdate WHERE d_yearmonthnum >= 199810) as t
+ON uv.visitYearMonth = t.d_yearmonthnum
+WHERE c.c_custkey <= 3
+GROUP BY c.c_name, c.c_mktsegment, t.prettyMonthYear, uv.yearMonthKey 
+ORDER BY c.c_name, c.c_mktsegment, uv.yearMonthKey  ASC;
